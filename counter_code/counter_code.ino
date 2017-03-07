6832e87f56c6e1b437c5c6b15d36a7dcddb05afb
Daniel Andrade <daniel@danielandradenet>
2017-03-07 19:39:11 +0100

    Initial commit

---
+++ counter_code/counter_code.ino
@@ -0,0 +1,30 @@
+/*
+
+	Days Countdown
+	Daniel Andrade _ www.danielandrade.net
+	2017
+
+	Code To Display Countdown to a specific date
+
+*/
+
+
+// Date of the countdown
+#define DAY 5
+#define MOMTH 2
+#define YEAR 2018
+#define HOUR 11
+#define MINUTE 00
+#define SECOND 00
+
+// Current Time
+
+void setup()
+{
+
+}
+
+void loop()
+{
+
+}
