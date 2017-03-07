42b8f2f3623c6d67df1dd8603ffb5a1820b11b8c
Daniel Andrade <daniel@danielandradenet>
2017-03-07 20:33:05 +0100

    add numbers array

--- counter_code/counter_code.ino
+++ counter_code/counter_code.ino
@@ -19,6 +19,19 @@
 
 // Current Time
 
+
+// Number Pins Setup (a,b,c,d,e,f,g)
+const uint8_t zero[7] 	= {1,1,1,1,1,1,0};
+const uint8_t one[7] 	= {0,1,1,0,0,0,0};
+const uint8_t two[7] 	= {1,1,0,1,1,0,1};
+const uint8_t three[7] 	= {1,1,1,1,0,0,1};
+const uint8_t four[7] 	= {0,1,1,0,0,1,1};
+const uint8_t five[7] 	= {1,0,1,1,0,1,1};
+const uint8_t six[7] 	= {1,0,1,1,1,1,1};
+const uint8_t seven[7] 	= {1,1,1,0,0,0,0};
+const uint8_t eihgt[7] 	= {1,1,1,1,1,1,1};
+const uint8_t nine[7] 	= {1,1,1,0,0,1,1};
+
 void setup()
 {
 
